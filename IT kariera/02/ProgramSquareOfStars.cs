0e6d34315772a6a610ad9b61829e73a7abbf1c5f
Tsvetoslav <cveti3060@gmail.com>
2022-01-30 16:22:42 +0200

    Solutions modil 1

---
+++ IT kariera/02/ProgramSquareOfStars.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            var n = int.Parse(Console.ReadLine());
+            if (n >= 2 && n <= 100)
+            {
+                string b = "*";
+                Console.Write(b);
+                for (i = 0; i < n - 2; i++)
+                {
+                    string c = " ";
+                    Console.Write(c);
+                }
+                Console.WriteLine("b");
+                
+            }
+
+
+        }
+    }
+}
+
